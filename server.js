aace0f00fcfac8a334bf94cd1e4006765d620641
Sol Lim <lims4010@gmail.com>
2020-02-08 18:34:16 -0500

    https certs

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
-
+const https = require('https');
+const fs = require('fs');
 const app = express();
 
 // Connect Database
@@ -25,4 +26,16 @@ app.set('trust proxy', true);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync('/etc/ssl/private/thankful-wall.key'),
+      cert: fs.readFileSync('/etc/ssl/certs/thankful-wall.crt')
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log('Listening...');
+  });
