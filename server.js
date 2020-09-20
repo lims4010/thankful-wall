4acedd9cfd85751b4540a26fb6ad42efb694728c
lims4010 <lims4010@gmail.com>
2020-09-20 11:15:20 -0400

    aws pipeline files

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
-const app = express();
+const app = express()
 
-// Connect Database
-connectDB();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-// Define Routes
-
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-
-// Serve static assets in production
-app.use(express.static('pages/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'pages', 'build', 'index.html'));
-});
-app.set('trust proxy', true);
-
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+
+// const connectDB = require('./config/db');
+// const path = require('path');
+// const app = express();
+
+// // Connect Database
+// connectDB();
+
+// // Init Middleware
+// app.use(express.json({ extended: false }));
+
+// // Define Routes
+
+// app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/auth', require('./routes/api/auth'));
+// app.use('/api/posts', require('./routes/api/posts'));
+
+// // Serve static assets in production
+// app.use(express.static('pages/build'));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'pages', 'build', 'index.html'));
+// });
+// app.set('trust proxy', true);
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
