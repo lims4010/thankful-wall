27da39516af7ff4b1945100d67209cd786b462dd
lims4010 <lims4010@gmail.com>
2020-09-21 21:04:50 -0400

    deployment debug

--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -4,10 +4,15 @@ cd /var/www/html/service_files
 
 sudo cp app.service /etc/systemd/system
 
-export mongoURI= aws ssm get-parameters --region us-east-1 --names /thankful-wall/mongoURI --query Parameters[0].Value
-export jwtSecret= aws ssm get-parameters --region us-east-1 --names /thankful-wall/jwtSecret --query Parameters[0].Value
+touch /var/www/html/service_files/appEnvVar.conf
+
+mongoURI=$(aws ssm get-parameters --region us-east-1 --names /thankful-wall/mongoURI --query Parameters[0].Value)
+jwtSecret=$(aws ssm get-parameters --region us-east-1 --names /thankful-wall/jwtSecret --query Parameters[0].Value)
+
+echo "mongoURI=$mongoURI" > /var/www/html/service_files/appEnvVar.conf
+echo "jwtSecret=$jwtSecret" >> /var/www/html/service_files/appEnvVar.conf
 
 sudo systemctl daemon-reload
-sudo systemctl start app
+sudo systemctl reload-or-restart app
 
 
