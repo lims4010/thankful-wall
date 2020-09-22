698093fbc83b36a7338eb0a61debc7e40440670b
lims4010 <lims4010@gmail.com>
2020-09-21 23:33:41 -0400

    deployment debug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update
-curl https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
-bash nodesource_setup.sh
+sudo curl https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
 sudo apt install nodejs
 sudo apt-get install nginx -y
 sudo apt-get install awscli -y
