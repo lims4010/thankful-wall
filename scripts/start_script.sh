c6a03fd827683dc27a6f47e57e58a419db75bbec
lims4010 <lims4010@gmail.com>
2020-09-21 21:14:05 -0400

    deployment debug

--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -4,7 +4,7 @@ cd /var/www/html/service_files
 
 sudo cp app.service /etc/systemd/system
 
-touch /var/www/html/service_files/appEnvVar.conf
+touch /etc/systemd/system/appEnvVar.conf
 
 mongoURI=$(aws ssm get-parameters --region us-east-1 --names /thankful-wall/mongoURI --query Parameters[0].Value)
 jwtSecret=$(aws ssm get-parameters --region us-east-1 --names /thankful-wall/jwtSecret --query Parameters[0].Value)
