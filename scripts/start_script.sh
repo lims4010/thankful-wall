c5660f646a40be84bc28e8796ebfb466dd700e74
lims4010 <lims4010@gmail.com>
2020-09-21 22:09:15 -0400

    deployment debug

--- scripts/start_script.sh
+++ scripts/start_script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Copy service file to main sytem
 cd /var/www/html/service_files
-
 sudo cp app.service /etc/systemd/system
 
+# Set env variables for service file
 touch /etc/systemd/system/appEnvVar.conf
 
 mongoURI=$(aws ssm get-parameters --region us-east-1 --names /thankful-wall/mongoURI --query Parameters[0].Value)
 jwtSecret=$(aws ssm get-parameters --region us-east-1 --names /thankful-wall/jwtSecret --query Parameters[0].Value)
 
-echo "mongoURI=$mongoURI" > /var/www/html/service_files/appEnvVar.conf
-echo "jwtSecret=$jwtSecret" >> /var/www/html/service_files/appEnvVar.conf
+echo "mongoURI=$mongoURI" > /etc/systemd/system/appEnvVar.conf
+echo "jwtSecret=$jwtSecret" >> /etc/systemd/system/appEnvVar.conf
 
+# Reload/Start service
 sudo systemctl daemon-reload
 sudo systemctl reload-or-restart app
 
